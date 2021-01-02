eddf07b003059bf5fcfc0753c37bb3b4ed0a9b97
Juan Pablo Diaz <juan.diaz.mart@gmail.com>
2021-01-02 14:52:06 -0500

    Update forloop.sh

--- Cmdstan/forloop.sh
+++ Cmdstan/forloop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# sbatch  -n 16 --mem-per-cpu=250M -t 01:00:00 -o $SCRATCH/test/test.out -J test $HOME/covid19_multi/Cmdstan/forloop.sh
+
+
 export STAN_NUM_THREADS=-1
 
 for i in {1..3}
       do
-      covid19_multi/Stan/ModeloJer2QRhosp_reduce sample data file=covid19_multi/Cmdstan/jer_2modi.json init=covid19_multi/Cmdstan/inits_${i}.json \
-      output file=covid19_multi/Cmdstan/jer_2modi_${i}.csv
+      $HOME/covid19_multi/Stan/ModeloJer2QRhosp_reduce sample data file=$HOME/covid19_multi/Cmdstan/jer_2modi.json init=$HOME/covid19_multi/Cmdstan/inits_${i}.json \
+      output file=$SCRATCH/test/jer_2modi_${i}.csv
     done
