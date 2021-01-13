a85db2532002599416db494b5835e3abacf6130a
janikaren <janikaren@ciencias.unam.mx>
2021-01-13 16:38:42 -0600

    UnaMasGraficas

--- FIG/graf1.R
+++ FIG/graf1.R
@@ -11,15 +11,12 @@ datos=readRDS("Data/datos.rds")
 mdat=datos$muerte
 hdat=datos$hosp
 
+
+
 mu_l1=fitJer2QRmodi_h$draws("mu_l")
 mu_l1=as_draws_matrix(mu_l1)
 mu_l1=as.data.frame(mu_l1)
 colnames(mu_l1)=levels(mdat$ENTIDAD_UM)
-
-mu_l2=fitJer2QRmodi_h$draws("mu_l2")
-mu_l2=as_draws_matrix(mu_l2)
-mu_l2=as.data.frame(mu_l2)
-cml2=colnames(mu_l2)=levels(mdat$SECENT)
 
 color_scheme_set("gray")
 mcmc_intervals(
@@ -33,6 +30,12 @@ mcmc_intervals(
     x="log hazard ratio"
   )
 
+
+
+mu_l2=fitJer2QRmodi_h$draws("mu_l2")
+mu_l2=as_draws_matrix(mu_l2)
+mu_l2=as.data.frame(mu_l2)
+cml2=colnames(mu_l2)=levels(mdat$SECENT)
 
 mcmc_intervals(
   mu_l2,
@@ -51,29 +54,6 @@ beta_m=as_draws_matrix(beta_m)
 beta_m=as.data.frame(beta_m)
 colnames(beta_m)=c("COPD","OBESITY","CHRONIC_KIDNEY","ASTHMA", "IMMUSUPR" )
 
-beta_h=fitJer2QRmodi_h$draws("beta_h")
-beta_h=as_draws_matrix(beta_h)
-beta_h=as.data.frame(beta_h)
-colnames(beta_h)=c("OBESITY","CHRONIC_KIDNEY")
-
-muraw_m=fitJer2QRmodi_h$draws("mu_raw_mort")
-muraw_m=as_draws_matrix(muraw_m)
-muraw_m=as.data.frame(muraw_m)
-
-
-muraw_h=fitJer2QRmodi_h$draws("mu_raw_hosp")
-muraw_h=as_draws_matrix(muraw_h)
-muraw_h=as.data.frame(muraw_h)
-
-
-y_rep_mort=fitJer2QRmodi_h$draws("y_mort_tilde")
-y_rep_mort=as_draws_matrix(y_rep_mort)
-
-
-y_rep_hosp=fitJer2QRmodi_h$draws("y_hosp_tilde")
-y_rep_hosp=as_draws_matrix(y_rep_hosp)
-
-color_scheme_set("gray")
 mcmc_areas(
   beta_m,
   regex_pars  =c("COPD","OBESITY","CHRONIC_KIDNEY","ASTHMA", "IMMUSUPR" ), 
@@ -82,9 +62,26 @@ mcmc_areas(
   point_est = "median"
 )+
   ggplot2::labs(
-  title = "Beta for Deceases",
-  subtitle = ""
-)
+    x="log hazard ratio"
+  )
+mcmc_intervals(
+  beta_m,
+  regex_pars  =c("COPD","OBESITY","CHRONIC_KIDNEY","ASTHMA", "IMMUSUPR" ), 
+  prob = 0.8,
+  prob_outer = 0.95,
+  point_est = "median"
+)+
+  ggplot2::labs(
+    x="log hazard ratio"
+  )
+
+
+
+
+beta_h=fitJer2QRmodi_h$draws("beta_h")
+beta_h=as_draws_matrix(beta_h)
+beta_h=as.data.frame(beta_h)
+colnames(beta_h)=c("OBESITY","CHRONIC_KIDNEY")
 
 mcmc_areas(
   beta_h,
@@ -94,10 +91,24 @@ mcmc_areas(
   point_est = "median"
 )+
   ggplot2::labs(
-    title = "Beta for Hospitalizations",
-    subtitle = ""
+    x="log hazard ratio"
+  )
+mcmc_intervals(
+  beta_h,
+  regex_pars=c("OBESITY","CHRONIC_KIDNEY"), 
+  prob = 0.8,
+  prob_outer = 0.95,
+  point_est = "median"
+)+
+  ggplot2::labs(
+    x="log hazard ratio"
   )
 
+
+
+muraw_m=fitJer2QRmodi_h$draws("mu_raw_mort")
+muraw_m=as_draws_matrix(muraw_m)
+muraw_m=as.data.frame(muraw_m)
 
 mcmc_areas(
   muraw_m,
@@ -107,10 +118,14 @@ mcmc_areas(
   point_est = "median"
 )+
   ggplot2::labs(
-    title = "Mu_raw for Deceases",
-    subtitle = ""
+    x="log hazard ratio"
   )
 
+
+
+muraw_h=fitJer2QRmodi_h$draws("mu_raw_hosp")
+muraw_h=as_draws_matrix(muraw_h)
+muraw_h=as.data.frame(muraw_h)
 
 mcmc_areas(
   muraw_h,
@@ -120,17 +135,26 @@ mcmc_areas(
   point_est = "median"
 )+
   ggplot2::labs(
-    title = "Mu raw for Hospitalizations",
-    subtitle = ""
+    x="log hazard ratio"
   )
 
 
-qplot(y_rep_hosp,geom="density", alpha=I(1/10),main = "Predictive Posterior Distribution Hosp",xlim=c(-1,50))+
-theme_bw() +scale_fill_manual(values=c("black"))
 
-qplot(y_rep_mort,geom="density", main = "Predictive Posterior Distribution Dead",xlim=c(-1,50))+
+y_rep_mort=fitJer2QRmodi_h$draws("y_mort_tilde")
+y_rep_mort=as_draws_matrix(y_rep_mort)
+
+qplot(y_rep_mort,geom="density",xlim=c(-1,50))+
   theme_bw() +scale_fill_manual(values=c("black"))
 
-plot(density(y_rep_mort), xlim=c(-1,70),main="Predictive Posterior Distribution Dead")
-plot(density(y_rep_hosp), xlim=c(-1,50),main="Posterior predictiva Hospitalización Hosp")
+plot(density(y_rep_mort), xlim=c(-1,70),main="")
+
+
+
+y_rep_hosp=fitJer2QRmodi_h$draws("y_hosp_tilde")
+y_rep_hosp=as_draws_matrix(y_rep_hosp)
+
+qplot(y_rep_hosp,geom="density", alpha=I(1/10),xlim=c(-1,50))+
+theme_bw() +scale_fill_manual(values=c("black"))
+
+plot(density(y_rep_hosp), xlim=c(-1,50))
 
