531781e2a91d7f969b0a7b753ad49025ef28eab2
Juan Pablo Diaz <juan.diaz.mart@gmail.com>
2021-02-03 15:16:22 -0500

    update

--- Cmdstan/forloop_jer2.sh
+++ Cmdstan/forloop_jer2.sh
@@ -6,7 +6,7 @@ echo "Job $SLURM_ARRAY_TASK_ID  start: $(date)"
 
 export STAN_NUM_THREADS=-1
 
-$HOME/covid19_multi/Stan/ModeloJer2QR_reduce sample num_samples=750 num_warmup=500  data file=$HOME/covid19_multi/Cmdstan/jer_2.json init=$HOME/covid19_multi/Cmdstan/inits_${SLURM_ARRAY_TASK_ID}.json \
+$HOME/covid19_multi/Stan/ModeloJer2QR_reduce sample num_samples=750 num_warmup=750  data file=$HOME/covid19_multi/Cmdstan/jer_2.json init=$HOME/covid19_multi/Cmdstan/inits_${SLURM_ARRAY_TASK_ID}.json \
 output file=$SCRATCH/multi/jer2_${SLURM_ARRAY_TASK_ID}.csv
 
 echo "Job $SLURM_ARRAY_TASK_ID  finish: $(date)"
