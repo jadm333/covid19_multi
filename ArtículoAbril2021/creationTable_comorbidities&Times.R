57018500f38ec6336119b93596837064bdaa2d53
Marco Gallegos <marcogallegos@ciencias.unam.mx>
2021-08-29 20:15:20 -0500

    creacion tableOne para hosp y muerte

--- ArtículoAbril2021/creationTable_comorbidities&Times.R
+++ ArtículoAbril2021/creationTable_comorbidities&Times.R
@@ -1,25 +1,43 @@
 source("./helpers/creation_df2.R")
 
+# También se puede cargar
+#df2 = read.csv("./df2_20210617.csv")
+
 library(tableone)
 
-myVars = c("tiempo_hosp",
-           "tiempo_muerte",
-           "DIABETES",
+set.seed(12345)
+
+hosp_na=df2 %>% filter(tiempo_hosp>1) %>% 
+  group_by(ENTIDAD_UM,SECTOR) %>% sample_frac(0.20) %>% ungroup()
+
+muerte_na=df2 %>% filter(
+  tiempo_muerte>=0,evento == 0) %>% 
+  group_by(ENTIDAD_UM,SECTOR) %>% sample_frac(0.20) %>% ungroup()
+
+
+##########################
+### tableOne para hosp ###
+##########################
+
+myVars_h = c("tiempo_hosp",
            "EPOC",
            "OBESIDAD",
-           "HIPERTENSION",
-           "SEXO",
            "RENAL_CRONICA",
-           "EDAD",
            "ASMA",
-           "INMUSUPR"
+           "INMUSUPR",
+           "SEXO",
+           "EDAD"
            )
 
+t_h = CreateTableOne(vars=myVars_h,strata="SECTOR",data=hosp_na,factorVars = c("EPOC",
+                                                                               "OBESIDAD",
+                                                                               "RENAL_CRONICA",
+                                                                               "ASMA",
+                                                                               "INMUSUPR",
+                                                                               "SEXO"))
+t2_h = as.data.frame(print(t_h, formatOptions = list(big.mark = ","),nonnormal = T,missing=T,test=F))
 
-t = CreateTableOne(vars=myVars,strata="SECTOR",data=df2)
-t2 = as.data.frame(print(t, formatOptions = list(big.mark = ","),nonnormal = T,missing=T,test=F))
-
-colnames(t2) = c("State managed",
+colnames(t2_h) = c("State managed",
                  "IMSS",
                  "ISSSTE",
                  "Private healthcare provider",
@@ -27,8 +45,53 @@ colnames(t2) = c("State managed",
                  "SSA",
                  "Missing data")
 
-rownames(t2) = c("n",
+rownames(t2_h) = c("n",
                  "Time from symptoms to hospitalization (median [IQR])",
+                 "COPD = NO (%)",
+                 "Obesity = NO (%)",
+                 "Chronic Kidney = NO (%)",
+                 "Asthma = NO (%)",
+                 "Immunosuppression = NO (%)",
+                 "Female = NO (%)",
+                 "Age = NO (%)"
+                 )
+
+#write.csv(t2_h,"./ArtículoAbril2021/tableOne-comorbidities-hosp.csv")
+
+
+############################
+### tableOne para muerte ###
+############################
+
+myVars_m = c("tiempo_muerte",
+             "DIABETES",
+             "EPOC",
+             "OBESIDAD",
+             "HIPERTENSION",
+             "SEXO",
+             "RENAL_CRONICA",
+             "EDAD"
+             )
+
+
+
+t_m = CreateTableOne(vars=myVars_m,strata="SECTOR",data=muerte_na,factorVars = c("DIABETES",
+                                                                                 "EPOC",
+                                                                                 "OBESIDAD",
+                                                                                 "HIPERTENSION",
+                                                                                 "SEXO",
+                                                                                 "RENAL_CRONICA"))
+t2_m = as.data.frame(print(t_m, formatOptions = list(big.mark = ","),nonnormal = T,missing=T,test=F))
+
+colnames(t2_m) = c("State managed",
+                 "IMSS",
+                 "ISSSTE",
+                 "Private healthcare provider",
+                 "SEDENA/SEMAR/PEMEX",
+                 "SSA",
+                 "Missing data")
+
+rownames(t2_m) = c("n",
                  "Time from hospitalization to death (median [IQR])",
                  "Diabetes = NO (%)",
                  "COPD = NO (%)",
@@ -36,9 +99,13 @@ rownames(t2) = c("n",
                  "Hypertension = NO (%)",
                  "Female = NO (%)",
                  "Chronic Kidney = NO (%)",
-                 "Age = NO (%)",
-                 "Asthma = NO (%)",
-                 "Immunosuppression = NO (%)"
+                 "Age = NO (%)"
                  )
 
-#write.csv(t2,"./ArtÃ­culoAbril2021/tableOne_comorbidities&Times.csv")
+#write.csv(t2_m,"./ArtículoAbril2021/tableOne-comorbidities-muerte.csv")
+
+
+
+### Deprecated ###
+
+#write.csv(t2,"./ArtículoAbril2021/tableOne_comorbidities&Times.csv")
